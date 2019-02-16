dee550c222f099f4ec578c5ae803075653c4d989
Abhay Singh Naurang <abhay_singh1@live.com>
2019-02-16 17:30:46 +0530

    Initial Commit

---
+++ YoutubeLiveVideoChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeLiveVideoChat
+//
+//  Created by Abhay Singh Naurang on 16/02/19.
+//  Copyright © 2019 Abhay Singh Naurang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
